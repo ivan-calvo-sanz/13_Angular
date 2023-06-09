8c199faaba9da6a5912fa76747ea24519b72b06a
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2023-06-09 18:22:51 +0200

    añado Videotutorial 12

---
+++ 1_Pildoras Informaticas CURSO ANGULAR/Videotutorial_12/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Mini Calculadora';
+  numero1: string = '0';
+  numero2: string = '0';
+  resultado: number = 0;
+
+  sumar(): void {
+    this.resultado = parseInt(this.numero1) + parseInt(this.numero2);
+  }
+  restar(): void {
+    this.resultado = parseInt(this.numero1) - parseInt(this.numero2);
+  }
+}
